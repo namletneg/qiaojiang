ab7a5a7af06d9e73ade0f7fdaf275a87341d6ee4
BigMarder <nameltneg@163.com>
2016-02-29 18:20:12 +0800

    首页修改

---
+++ static/js/main.js
@@ -0,0 +1,38 @@
+(function ($) {
+    // 导航
+    $('.touch').on('touchstart', function () {
+        $('.nav').width(window.innerWidth).toggle();
+    });
+
+    var $timber = $('.timber'),
+        left;
+    left = $timber.css('left');
+    $('.nav li').hover(function () {
+        var index = $(this).index('.nav li');
+
+        $timber.css('left', 92 * index + 60);
+    }, function () {
+        $timber.css('left', left);
+    });
+
+    // banner
+    var carousel = function () {
+            var $item = $('.carousel').find('.box .item'),
+                $active = $('.carousel').find('.box .active'),
+                $dotItem = $('.carousel').find('.dot a'),
+                $dot = $('.carousel').find('.dot .active');
+
+            $active.hide().removeClass('active');
+            $dot.removeClass('active');
+            if($active.next().length){
+                $active.next().show().addClass('active');
+                $dot.next().addClass('active');
+            } else{
+                $($item[0]).show().addClass('active');
+                $($dotItem[0]).addClass('active');
+            }
+        },
+        timer = setInterval(function () {
+            carousel();
+        }, 5000);
+})(jQuery);
